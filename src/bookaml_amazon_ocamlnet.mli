1877227447bf7d2780811a9fa97632ca12f2d74a
darioteixeira <darioteixeira@yahoo.com>
2014-08-06 14:18:00 +0100

    Update copyright dates

--- src/bookaml_amazon_ocamlnet.mli
+++ src/bookaml_amazon_ocamlnet.mli
@@ -1,6 +1,6 @@
 (********************************************************************************)
 (*	Bookaml_amazon_ocamlnet.mli
-	Copyright (c) 2010-2012 Dario Teixeira (dario.teixeira@yahoo.com)
+	Copyright (c) 2010-2014 Dario Teixeira (dario.teixeira@yahoo.com)
 	This software is distributed under the terms of the GNU GPL version 2.
 	See LICENSE file for full license text.
 *)
